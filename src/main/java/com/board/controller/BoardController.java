d26a19e058206257fc3376ee08c1ad4a8ab8f4b4
SHIN SUJUNG <shinssjj79@gmail.com>
2024-06-01 12:49:46 +0900

    Add paging functionality to BoardController
    - Added paging method to BoardController to support pagination of board entries.
    - Implemented pagination logic to determine start and end pages for display.
    - Updated the corresponding view to show paginated board entries.

--- src/main/java/com/board/controller/BoardController.java
+++ src/main/java/com/board/controller/BoardController.java
@@ -3,7 +3,9 @@ package com.board.controller;
 import com.board.dto.BoardDTO;
 import com.board.service.BoardService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.Banner;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -64,5 +66,29 @@ public class BoardController {
     public String delete(@PathVariable Long id) {
         boardService.delete(id);
         return "redirect:/board/";
+    }
+
+    //  /board/paging?page=1
+    @GetMapping("/paging")
+    public String paging(@PageableDefault(page = 1)Pageable pageable, Model model) {
+        pageable.getPageNumber();
+        Page<BoardDTO> boardList = boardService.paging(pageable);
+        int blockLimit = 3;
+        int startPage = (((int)(Math.ceil((double)pageable.getPageNumber() / blockLimit))) -1) * blockLimit;
+        int endPage = ((startPage + blockLimit -1) < boardList.getTotalPages()) ? startPage + blockLimit -1 : boardList.getTotalPages();
+
+        // Page 개수 20개
+        // 현재 사용자가 3페이지
+        // 1 2 3
+        // 현재 사용자가 7페이지
+        // 7 8 9
+        // 보여지는 페이지 개수 3개
+        // 총 페이지 개수 8개
+
+        model.addAttribute("boardList",boardList);
+        model.addAttribute("startPage",startPage);
+        model.addAttribute("endPage",endPage);
+        return "paging";
+
     }
 }
