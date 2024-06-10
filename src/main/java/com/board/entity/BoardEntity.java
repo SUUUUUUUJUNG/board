22d5e114087dbaed9ea4fb65774c80be18b2e442
SHIN SUJUNG <shinssjj79@gmail.com>
2024-06-10 19:38:36 +0900

    Added one-to-many relationship with BoardFileEntity in BoardEntity.
    - Added a one-to-many relationship mapping with BoardFileEntity.
    - Enabled cascading delete and orphan removal for BoardFileEntity.
    - Implemented lazy loading for the boardFileEntityList.
    - Added new method `toSaveFileEntity` for saving entities with attached files.

--- src/main/java/com/board/entity/BoardEntity.java
+++ src/main/java/com/board/entity/BoardEntity.java
@@ -4,6 +4,10 @@ import com.board.dto.BoardDTO;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.util.Lazy;
+
+import java.util.ArrayList;
+import java.util.List;
 
 // DB의 테이블 역할을 하는 클래스
 @Entity
@@ -34,6 +38,9 @@ public class BoardEntity extends BaseEntity {
     @Column
     private  int fileAttached; // 1 or 0
 
+    @OneToMany(mappedBy = "boardEntity", cascade = CascadeType.REMOVE, orphanRemoval = true, fetch = FetchType.LAZY)
+    private List<BoardFileEntity> boardFileEntityList = new ArrayList<>();
+
     public static BoardEntity toSaveEntity(BoardDTO boardDTO) {
         BoardEntity boardEntity = new BoardEntity();
         boardEntity.setBoardWriter(boardDTO.getBoardWriter());
@@ -53,6 +60,17 @@ public class BoardEntity extends BaseEntity {
         boardEntity.setBoardTitle(boardDTO.getBoardTitle());
         boardEntity.setBoardContents(boardEntity.getBoardContents());
         boardEntity.setBoardHits(boardDTO.getBoardHits());
+        return boardEntity;
+    }
+
+    public static BoardEntity toSaveFileEntity(BoardDTO boardDTO) {
+        BoardEntity boardEntity = new BoardEntity();
+        boardEntity.setBoardWriter(boardDTO.getBoardWriter());
+        boardEntity.setBoardPass(boardDTO.getBoardPass());
+        boardEntity.setBoardTitle(boardDTO.getBoardTitle());
+        boardEntity.setBoardContents(boardEntity.getBoardContents());
+        boardEntity.setBoardHits(0);
+        boardEntity.setFileAttached(1); // 파일 있음.
         return boardEntity;
     }
 }
