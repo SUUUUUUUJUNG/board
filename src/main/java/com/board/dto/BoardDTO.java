7b274646dddf34750abd7f9c134a0676b4d328cd
SHIN SUJUNG <shinssjj79@gmail.com>
2024-06-22 10:32:19 +0900

    Refactored BoardDTO to support multiple file attachments
    - Changed `originalFileNmae` and `storedFileName` from String to List<String> to handle multiple file names.
    - Updated `toBoardDTO` method to populate lists for original and stored file names when files are attached.
    - Updated constructors and methods to align with these changes.

--- src/main/java/com/board/dto/BoardDTO.java
+++ src/main/java/com/board/dto/BoardDTO.java
@@ -1,10 +1,12 @@
 package com.board.dto;
 
 import com.board.entity.BoardEntity;
+import com.board.entity.BoardFileEntity;
 import lombok.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
@@ -23,8 +25,8 @@ public class BoardDTO {
     private LocalDateTime boardUpdatedTime;
 
     private List<MultipartFile> boardFile; // save.html -> Controller 파일 담는 용도
-    private String originalFileNmae; // 원본 파일 이름
-    private String storedFileName; //서버 저장용 파일 이름
+    private List<String> originalFileNmae; // 원본 파일 이름
+    private List<String> storedFileName; //서버 저장용 파일 이름
     private int fileAttached; // 파일 첨부 여부(첨부 1, 미첨부 0)
 
     public static BoardDTO toBoardDTO(BoardEntity boardEntity) {
@@ -40,13 +42,15 @@ public class BoardDTO {
         if(boardEntity.getFileAttached() == 0) {
             boardDTO.setFileAttached(boardEntity.getFileAttached()); // 0
         } else {
+            List<String> originalFileNameList = new ArrayList<>();
+            List<String> storedFileNameList = new ArrayList<>();
             boardDTO.setFileAttached(boardEntity.getFileAttached()); // 1
-            // 파일 이름을 가져가야 함.
-            // originalFileName, storedFileName : board_file_table(BoardFileEntity)
-            // join
-            // select * from board_table b, board_file_table bf where b.id-bf.bord_id and where b.id=?
-            boardDTO.setOriginalFileNmae(boardEntity.getBoardFileEntityList().get(0).getOriginalFileName());
-            boardDTO.setStoredFileName(boardEntity.getBoardFileEntityList().get(0).getStoredFileName());
+            for(BoardFileEntity boardFileEntity: boardEntity.getBoardFileEntityList()) {
+                originalFileNameList.add(boardFileEntity.getOriginalFileName());
+                storedFileNameList.add(boardFileEntity.getStoredFileName());
+            }
+            boardDTO.setOriginalFileNmae(originalFileNameList);
+            boardDTO.setStoredFileName(storedFileNameList);
         }
         return boardDTO;
     }
