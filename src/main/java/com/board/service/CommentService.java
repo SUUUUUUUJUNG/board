53844e6896490d47f41c405ae1cb353f5177bf32
SHIN SUJUNG <shinssjj79@gmail.com>
2024-06-27 20:18:19 +0900

    Add CommentService class to manage comments
    - Added package `com.board.service`
    - Implemented `CommentService` class
    - Added necessary annotations: `@Service` and `@RequiredArgsConstructor`
    - Injected `CommentRepository` as a final variable in `CommentService`

---
+++ src/main/java/com/board/service/CommentService.java
@@ -0,0 +1,14 @@
+package com.board.service;
+
+import com.board.dto.CommentDTO;
+import com.board.repository.CommentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CommentService {
+
+    private final CommentRepository commentRepository;
+
+}
