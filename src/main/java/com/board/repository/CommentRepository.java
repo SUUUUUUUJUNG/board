e8c9930ffdd9f0dc1c0df1fcdd045530f6c8c8b0
SHIN SUJUNG <shinssjj79@gmail.com>
2024-07-04 20:23:51 +0900

    Add method to CommentRepository for fetching comments by BoardEntity
    - Imported BoardEntity class in CommentRepository.
    - Added method findAllByBoardEntityOrderByIdDesc to retrieve comments ordered by ID in descending order based on BoardEntity.

--- src/main/java/com/board/repository/CommentRepository.java
+++ src/main/java/com/board/repository/CommentRepository.java
@@ -1,7 +1,11 @@
 package com.board.repository;
 
+import com.board.entity.BoardEntity;
 import com.board.entity.CommentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CommentRepository extends JpaRepository<CommentEntity, Long> {
+    List<CommentEntity> findAllByBoardEntityOrderByIdDesc(BoardEntity boardEntity);
 }
