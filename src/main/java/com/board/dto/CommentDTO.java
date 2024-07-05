f707b1936fce333db49aeecedca190f34263e441
SHIN SUJUNG <shinssjj79@gmail.com>
2024-07-05 19:23:20 +0900

    Add conversion method to CommentDTO
    - Added import for CommentEntity
    - Added static method toCommentDTO to convert CommentEntity to CommentDTO
    - Set boardId parameter separately due to potential issues with @Transactional in Service method
    
    Refactored CommentDTO class to include conversion method for CommentEntity, making data transfer between layers more efficient.

--- src/main/java/com/board/dto/CommentDTO.java
+++ src/main/java/com/board/dto/CommentDTO.java
@@ -1,9 +1,11 @@
 package com.board.dto;
 
+import com.board.entity.CommentEntity;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import javax.xml.stream.events.Comment;
 import java.time.LocalDateTime;
 
 @Getter
@@ -15,4 +17,15 @@ public class CommentDTO {
     private String commentContents;
     private Long boardId;
     private LocalDateTime commentCreatedTime;
+
+    public static CommentDTO toCommnetDTO(CommentEntity commentEntity, Long boardId) {
+        CommentDTO commentDTO = new CommentDTO();
+        commentDTO.setId(commentEntity.getId());
+        commentDTO.setCommentWriter(commentEntity.getCommentWriter());
+        commentDTO.setCommentContents(commentEntity.getCommentContents());
+        commentDTO.setCommentCreatedTime(commentEntity.getCreatedTime());
+        //commentDTO.setBoardId(commentEntity.getBoardEntity().getId()); // 이 구조를 사용하려면 Service 메서드에 @Transactional을 붙여줘야함.
+        commentDTO.setBoardId(boardId);
+        return commentDTO;
+    }
 }
